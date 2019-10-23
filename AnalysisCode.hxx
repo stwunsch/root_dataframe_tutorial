adff9551e1cd8ee9541a32c70cfa294e8a0e8a08
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-10-23 14:10:53 +0200

    rename RVec typedef to Vec_t

--- AnalysisCode.hxx
+++ AnalysisCode.hxx
@@ -4,8 +4,8 @@
 // Use O3 optimization level for just-in-time compilation
 #pragma cling optimize(3)
 
-using RVec = const ROOT::VecOps::RVec<float>&;
-float compute_mass(RVec pt, RVec eta, RVec phi, RVec mass) {
+using Vec_t = const ROOT::VecOps::RVec<float>&;
+float compute_mass(Vec_t pt, Vec_t eta, Vec_t phi, Vec_t mass) {
     ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], mass[0]);
     ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], mass[1]);
     return (p1 + p2).mass();
